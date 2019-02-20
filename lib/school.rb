53f0011cae5ed51d3051c2d2598b8ecdb37a3028
CalieR <calielrushton@gmail.com>
2019-02-20 15:14:32 +0000

    one test left to pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+# School class is a model in domain context
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    if roster.has_key?(grade)
+      roster[grade]
+    end
+  end
+
+  def sort
+    # iterate over roster
+     roster[grade].transform_values { |arr| arr.sort }
+    # roster[grade].sort
+  end
+
+# binding.pry
+
+end
